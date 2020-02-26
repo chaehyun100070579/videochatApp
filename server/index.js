960574a0e1b62ab0d2b9f2aff110a8e76f277320
chaehyun100070579 <32354521+chaehyun100070579@users.noreply.github.com>
2020-02-26 18:09:14 +0900

    delet eclient module

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!~~ '))
-app.get('/api/hello', (req, res) => res.send('Hello World!~~'))
+// app.get('/', (req, res) => res.send('Hello World!~~ '))
+// app.get('/api/hello', (req, res) => res.send('Hello World!~~'))
 
-const port = 5000
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.use(express.static(__dirname + '/build'))
+app.get('/', (req, res, next) => {
+    res.sendFile(__dirname + '/build/index.html')
+})
 
 var io = require('socket.io')
 (
@@ -13,3 +16,6 @@ var io = require('socket.io')
         path: '/webrtc'
     }
 )
+
+const port = 5000
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
