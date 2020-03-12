824228c39a4bca20339eff08a0a12c5082c55e45
chaehyun100070579 <32354521+chaehyun100070579@users.noreply.github.com>
2020-03-12 10:54:41 +0900

    LandingPage chatoptionbox interface added

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import './LandingPage.css'
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+    chatOptionBox: {
+      '& > *': {
+        width: 400,
+        margin: 10
+      },
+    },
+    ChatRoomButton: {
+        width: 70,
+        padding: '14.5px',
+    },
+  }));
 
 function LandingPage(props) {
+    const classes = useStyles();
+    const [interestsArr, setInterestsArr] = useState([]);
+
     return (
         <div>
-            LandingPage
+            <h1 className='LandingPageTitle'>Live Chat</h1>
+            <div className={classes.chatOptionBox} noValidate autoComplete="off">
+                <TextField id="outlined-basic" label="Add your Interests (optional)" variant="outlined" />
+                <Button className={classes.ChatRoomButton} variant="contained" color="primary" href="ChatRoomPage">
+                    Text
+                </Button>
+                <span style={{margin: '0'}}>or</span>
+                <Button className={classes.ChatRoomButton} variant="contained" color="primary" href="ChatRoomPage">
+                    Video
+                </Button>
+            </div>
         </div>
     );
 }
