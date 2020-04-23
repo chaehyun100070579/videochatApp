4e88e8044dbc0567e513df969df830dece170170
chaehyun100070579 <32354521+chaehyun100070579@users.noreply.github.com>
2020-04-23 16:18:51 +0900

    :construction: add stop button with esc event

--- client/src/components/views/TextChatRoom/TextChatRoom.js
+++ client/src/components/views/TextChatRoom/TextChatRoom.js
@@ -15,7 +15,8 @@ function TextChatRoom({location}) {
     const ENDPOINT = 'localhost:5000';
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-
+    var [stopButtonText, setStopButtonText] = useState('Stop');
+   
     useEffect(() =>{
         const {interests} = queryString.parse(location.search);
         setInteretString(interests)
@@ -23,54 +24,70 @@ function TextChatRoom({location}) {
         socket = io(ENDPOINT);
       
         socket.emit('join', {interests}, () =>{
-           
         });
-
+        
+        socket.on('message', message => {
+            setMessages(messages => [ ...messages, message ]);
+        });
+          
         return () => {
             socket.emit('disconnect')
             socket.off();
         }
     },[ENDPOINT, location.search]);
-
-    // useEffect(() =>{
-    //     socket.on('message', (message) => {
-    //         setMessages([...messages, message])
-    //     })
-
-    // }, [messages]);
-
-    useEffect(() => {
-        socket.on('message', message => {
-          setMessages(messages => [ ...messages, message ]);
-        });
-    }, []);
-
+ 
+    // useEffect(() => {
+    //     socket.on('message', message => {
+    //       setMessages(messages => [ ...messages, message ]);
+    //     });
+    // }, []);
     
     const sendMessage = (event) => {
-        console.log(event.key)
         event.preventDefault();
         if(message){
             socket.emit('sendMessage', message, () => setMessage(''));
         }
     } 
+
+    useEffect(() => {
+        document.addEventListener("keydown", backToHome, false);
+
+        return () => {
+            document.removeEventListener("keydown", backToHome, false);
+        };
+    });
+   
+    const backToHome = (event) => {
+        if(stopButtonText === 'Really'){
+            setStopButtonText(stopButtonText='New')
+            return;
+        }
+        if(event.keyCode === 27) {
+            console.log(event.keyCode);
+            setStopButtonText(stopButtonText='Really')
+        }
+    } 
+
     console.log(message, messages);
 
     return (
-        <div className="outerContainer">
+        <div className="outerContainer" >
             <div className="container">
-          
                 <div className ="logWrapper">
                     <InfoBar/>
-
                     <span>You're now chatting with a random stranger</span> 
                     {interestString && <span className="interestTitle">you both like {interestString} </span>}
-                   
                     <ScrollToBottom className="messageLog">
                             <Messages messages={messages} name='stranger' />
                     </ScrollToBottom>
-               
                 </div>
                 <div className="messageWrapper">
+                            <a href="/">
+                                <button className="stopButton" id={stopButtonText}>
+                                     {stopButtonText}
+                                    <span className="btnshortcut">Esc</span>
+                                </button>
+                            </a>
                     <textarea 
                         className="messageTextArea"
                         placeholder="Type a message..."
@@ -80,7 +97,7 @@ function TextChatRoom({location}) {
                     />
                     <button className="sendButton" onClick={ (event) => sendMessage(event)}>
                         SEND
-                        <div className="btnkbshortcut">Enter</div>
+                        <span className="btnshortcut">Enter</span>
                     </button>
                 </div>
             </div>
